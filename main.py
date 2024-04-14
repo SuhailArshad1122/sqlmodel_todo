192bbd2497c8d53b2b0a81fb96516e67e6338699
Danial Saleemi <salimicse@gmail.com>
2024-04-14 21:26:30 +0500

    executes uvicorn command

--- main.py
+++ main.py
@@ -1,69 +1,7 @@
-from typing import Optional
-from fastapi import FastAPI
-# from contextlib import asynccontextmanager
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+import uvicorn
 
 
-class Task(SQLModel, table=True):
-    id: Optional[int]  = Field(default=None, primary_key=True)
-    content: str = Field(index=True)
+print('main function invoked')
 
-
-
-sqlite_url = f"postgresql://lifcon.solutions:G2gSjnH5Bbal@ep-white-violet-581515-pooler.ap-southeast-1.aws.neon.tech/mate_sqlmodel?sslmode=require"
-
-
-engine = create_engine(sqlite_url)
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     create_db_and_tables()
-#     yield
-
-app: FastAPI = FastAPI()
-# app: FastAPI = FastAPI(lifespan=lifespan)
-
-
-
-@app.post("/task/")
-def create_task(task: Task):
-    with Session(engine) as session:
-        session.add(task)
-        session.commit()
-        session.refresh(task)
-        return task
-
-@app.put("/task/")
-def update_task(task: Task):
-    with Session(engine) as session:
-        statement = select(Task).where(Task.id == task.id)
-        results = session.exec(statement)
-        db_task = results.one()
-
-        db_task.content = task.content
-        session.add(db_task)
-        session.commit()
-        session.refresh(db_task)
-        return db_task
-
-
-@app.get("/task/")
-def read_task():
-    with Session(engine) as session:
-        tasks = session.exec(select(Task)).all()
-        return tasks
-
-@app.delete("/task/")
-def delete_task(task: Task):
-    with Session(engine) as session:
-        statement = select(Task).where(Task.id == task.id)
-        results = session.exec(statement)
-        db_task = results.one()
-
-        session.delete(db_task)
-        session.commit()
-        return "Your task has been deleted .... "
+if __name__ == '__main__':
+    uvicorn.run('src.api:app', reload=True)
